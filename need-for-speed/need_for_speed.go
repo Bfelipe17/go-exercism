bce88c8f682f1bff840ede46905e8f64bb9268a0
Bfelipe17 <bruno.felipe17@hotmail.com>
2024-04-25 22:40:08 -0300

    need-for-speed

--- need-for-speed/need_for_speed.go
+++ need-for-speed/need_for_speed.go
@@ -11,9 +11,9 @@ type Car struct {
 // NewCar creates a new remote controlled car with full battery and given specifications.
 func NewCar(speed, batteryDrain int) Car {
 	return Car{
-		speed:        speed,
-		batteryDrain: batteryDrain,
 		battery:      100,
+		batteryDrain: batteryDrain,
+		speed:        speed,
 		distance:     0,
 	}
 }
@@ -34,14 +34,18 @@ func NewTrack(distance int) Track {
 // the car will not move.
 func Drive(car Car) Car {
 	if car.battery >= car.batteryDrain {
-		car.battery -= car.batteryDrain
-		car.distance += car.speed
+		return Car{
+			battery:      car.battery - car.batteryDrain,
+			speed:        car.speed,
+			batteryDrain: car.batteryDrain,
+			distance:     car.distance + car.speed,
+		}
 	}
+
 	return car
 }
 
 // CanFinish checks if a car is able to finish a certain track.
 func CanFinish(car Car, track Track) bool {
-	max := (car.battery / car.batteryDrain) * car.speed
-	return max >= track.distance
+	return ((car.battery/car.batteryDrain)*car.speed)+car.distance >= track.distance
 }
